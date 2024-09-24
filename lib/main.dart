39175b639dad6ad35347c8be734780b24584b469
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-09-24 16:02:59 +0900

    feat: Webtoon app

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xffe7626c),
-        textTheme: const TextTheme(
-          headlineLarge: TextStyle(
-            color: Color(0xff232b55),
-          ),
-        ),
-        cardColor: const Color(0xfff4eddb),
-      ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
